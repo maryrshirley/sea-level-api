502b223120534cdfe13c79dd3fda5871aabb07be
Paul M Furley <paul@paulfurley.com>
2015-03-05 11:47:10 +0000

    Give a JSON 404 when no tide gauge is specified
    When you access the `/1/raw-measurements/tide-gauges/` endpoint without
    specifying a tide gauge slug, it's nicer to get a JSON error explaining
    why, rather than an HTML 404.

--- api/apps/tide_gauges/urls.py
+++ api/apps/tide_gauges/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 
-from .views import RawMeasurements
+from .views import RawMeasurements, RawMeasurementsIndex
 
 urlpatterns = patterns(
     '',
+
+    url(r'^raw-measurements/$',
+        RawMeasurementsIndex.as_view(),
+        name='tide-gauge-raw-masurements-index'),
 
     url(r'^raw-measurements/(?P<tide_gauge_slug>' +
         settings.SLUG_REGEX + ')/$',
