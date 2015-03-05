1239dd1dc08a696c3fc4f257ff6a3a8c11c0b2a4
Paul M Furley <paul@paulfurley.com>
2015-03-05 12:14:07 +0000

    Implement RawMeasurement read view
    [Delivers #89016198]

--- api/apps/tide_gauges/views/raw_measurements.py
+++ api/apps/tide_gauges/views/raw_measurements.py
@@ -1,5 +1,12 @@
+import datetime
+
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+
+from api.libs.param_parsers import (parse_time_range, MissingParameterError,
+                                    ObjectNotFoundError, TimeRangeError)
 
 from api.libs.json_envelope_renderer import replace_json_renderer
 
@@ -46,3 +53,38 @@ class RawMeasurements(ListCreateAPIView):
             return dict(list(a.items()) + list(b.items()))
 
         return combine_dicts(c, {'tide_gauge': tide_gauge})
+
+    def get_queryset(self):
+        return parse_and_get_queryset(
+            self.kwargs.get('tide_gauge_slug', None),
+            self.request.query_params.get('start', None),
+            self.request.query_params.get('end', None))
+
+
+def parse_and_get_queryset(tide_gauge_or_none, start_or_none, end_or_none):
+    tide_gauge = get_tide_gauge(tide_gauge_or_none)
+    time_range = parse_time_range(start_or_none, end_or_none)
+
+    validate_time_range(time_range)
+
+    return RawMeasurement.objects.filter(
+        tide_gauge=tide_gauge,
+        datetime__gte=time_range.start,
+        datetime__lt=time_range.end)
+
+
+def get_tide_gauge(slug_or_none):
+    if slug_or_none is None:
+        raise MissingParameterError('No tide gauge specified in URL.')
+
+    try:
+        return TideGauge.objects.get(slug=slug_or_none)
+    except ObjectDoesNotExist:
+        raise ObjectNotFoundError(
+            'Tide gauge not found: `{}`.'.format(slug_or_none))
+
+
+def validate_time_range(time_range):
+    if time_range.end - time_range.start > datetime.timedelta(hours=24):
+        raise TimeRangeError('`start` and `end` must be no greater than '
+                             '24 hours apart')
