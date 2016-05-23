d0f0b179148b878b8463801b54f344e58e56c942
Peter Brooks <mail@pbrooks.net>
2016-05-23 15:07:45 +0100

    Notifications: Drop location, use vessel name instead

--- api/apps/notifications/serializers.py
+++ api/apps/notifications/serializers.py
@@ -5,8 +5,8 @@ from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
 
-    location = serializers.SlugRelatedField(slug_field='slug', read_only=True)
+    vessel = serializers.SlugRelatedField(slug_field='name', read_only=True)
 
     class Meta:
         model = Notification
-        fields = ('location', 'timestamp', 'category', 'status', )
+        fields = ('timestamp', 'category', 'status', 'vessel')
