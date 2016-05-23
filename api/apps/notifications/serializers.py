eaed7a1f796959901d1680a7f1bd5b94d6a1a721
Peter Brooks <mail@pbrooks.net>
2016-05-23 15:03:19 +0100

    Notification: Switch out location.id to slug

--- api/apps/notifications/serializers.py
+++ api/apps/notifications/serializers.py
@@ -5,6 +5,8 @@ from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
 
+    location = serializers.SlugRelatedField(slug_field='slug', read_only=True)
+
     class Meta:
         model = Notification
         fields = ('location', 'timestamp', 'category', 'status', )
